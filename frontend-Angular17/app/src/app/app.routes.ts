0fd2f4de5d1e143b7a99c8bc987205536a987717
Yarex17 <104018313+Yarex17@users.noreply.github.com>
2024-03-30 01:28:21 -0600

    Galeria de imagenes

--- frontend-Angular17/app/src/app/app.routes.ts
+++ frontend-Angular17/app/src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { DashboardComponent } from "./dashboard/dashboard.component";
 import { InicioComponent } from "./inicio/inicio.component";
 import { ReservarComponent } from "./reservar/reservar.component";
 import { ComoLlegarComponent } from "./como-llegar/como-llegar.component";
+import { FacilidadesComponent } from "./facilidades/facilidades.component";
+import { SobreNosotrosComponent } from "./sobre-nosotros/sobre-nosotros.component";
 
 export const routes : Routes =[
     {path: "dashboard", component :  DashboardComponent},
     {path: "reservar", component :  ReservarComponent},
     {path: "comollegar", component: ComoLlegarComponent},
+    {path: "facilidades", component :  FacilidadesComponent},
+    {path: "sobreNosotros", component :  SobreNosotrosComponent},	
     {path: "", component :  InicioComponent}
 ]
