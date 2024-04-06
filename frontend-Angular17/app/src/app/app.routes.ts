5db25ca2bed329d5388850933509565fc995df9d
Kevin080899 <kevinaguilarmasis@gmail.com>
2024-04-05 23:36:24 -0600

    imports
    comment

--- frontend-Angular17/app/src/app/app.routes.ts
+++ frontend-Angular17/app/src/app/app.routes.ts
@@ -2,6 +2,10 @@ import { Routes } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { InicioComponent } from "./inicio/inicio.component";
 import { ReservarComponent } from "./reservar/reservar.component";
+import { SobreNosotrosComponent } from "./sobre-nosotros/sobre-nosotros.component";
+import { ContactenosComponent } from "./contactenos/contactenos.component";
+import { FacilidadesComponent } from "./facilidades/facilidades.component";
+import { TarifasComponent } from "./tarifas/tarifas.component";
 
 import { ComoLlegarComponent } from "./como-llegar/como-llegar.component";
 
