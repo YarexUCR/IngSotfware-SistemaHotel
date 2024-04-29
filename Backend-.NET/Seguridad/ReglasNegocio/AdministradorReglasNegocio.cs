d5d7766e8517b708fda2b5048bf3ba101b72a579
Yarex17 <159191461+YarexUCR@users.noreply.github.com>
2024-04-29 10:27:57 -0600

    Api

---
+++ Backend-.NET/Seguridad/ReglasNegocio/AdministradorReglasNegocio.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Datos;
+
+namespace ReglasNegocio
+{
+    public class AdministradorReglasNegocio
+    {
+        private AdmnistradorDatos datos;
+
+        public AdministradorReglasNegocio(IConfiguration configuration)
+        {
+            datos = new AdmnistradorDatos(configuration);
+        }
+
+        public string ObtenerNombreAdministrador(string nombreUsuario, string contrasena) {
+            return datos.ObtenerNombreAdministrador( nombreUsuario,  contrasena);
+        }
+    }
+}
