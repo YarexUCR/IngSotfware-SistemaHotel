3b8806208428ced5677638f6731a5c9070761ba9
Yarex17 <159191461+YarexUCR@users.noreply.github.com>
2024-04-30 00:05:10 -0600

    ddddd

---
+++ Backend-.NET/Nucleo/Datos/ReservaDatos.cs
@@ -0,0 +1,34 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class ReservaDatos
+    {
+        private List<Reserva> reservas;
+
+        public ReservaDatos() {
+            this.reservas = new List<Reserva>();
+            reservas.Add(new Reserva());
+        }
+
+        public List<Reserva> obtenerReservas()
+        {
+            return this.reservas;
+        }
+
+        public bool agregarReserva(Reserva reserva) {
+            int tamaño = this.reservas.Count;
+            this.reservas.Add(reserva);
+            if (tamaño != this.reservas.Count) { 
+                return true;
+            }else { 
+                return false; 
+            }
+        }
+    }
+}
---
+++ Backend-.NET/Nucleo/Dominio/Habitacion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Habitacion
+    {
+        public int id;
+        public string estado;
+        public int numero;
+        public TipoHabitacion tipo;
+        public bool activo;
+
+        public Habitacion() { 
+            this.id = 0;
+            this.estado = "";
+            this.numero = 0;
+            this.tipo = new TipoHabitacion();
+            this.activo = false;
+        }
+
+        public Habitacion(int id, string estado, int numero, TipoHabitacion tipo, bool activo)
+        {
+            this.id = id;
+            this.estado = estado;
+            this.numero = numero;
+            this.tipo = tipo;
+            this.activo = activo;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Estado { get => estado; set => estado = value; }
+        public bool Activo { get => activo; set => activo = value; }
+        public int Numero { get => numero; set => numero = value; }
+        public TipoHabitacion Tipo { get => tipo; set => tipo = value; }
+    }
+}
