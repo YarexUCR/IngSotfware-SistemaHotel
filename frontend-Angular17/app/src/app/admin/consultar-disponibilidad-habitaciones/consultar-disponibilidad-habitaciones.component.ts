90072d6112204212bf20578a9e5f2a1fe24b632a
kjgCH1 <cheskyto22@gmail.com>
2024-05-02 02:50:10 -0600

    avances us 22
    llamado al servicio listo

--- frontend-Angular17/app/src/app/admin/consultar-disponibilidad-habitaciones/consultar-disponibilidad-habitaciones.component.ts
+++ frontend-Angular17/app/src/app/admin/consultar-disponibilidad-habitaciones/consultar-disponibilidad-habitaciones.component.ts
@@ -3,9 +3,17 @@ import { FooterComponent } from "../../footer/footer.component";
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { TipoHabitacion } from '../../dominio/TipoHabitacion';
-import { FormsModule, NgForm, Validators} from '@angular/forms';
+import { FormsModule, NgForm, Validators } from '@angular/forms';
 import { TipoHabitacionService } from '../../api/tipo.habitacion.service';
+import { Habitacion } from '../../dominio/Habitacion';
 
+import { differenceInDays, parseISO } from 'date-fns';
+
+interface ResultadoConsulta {
+  numeroHabitacion: number,
+  tipo: string,
+  costo: number
+}
 
 
 
@@ -14,20 +22,22 @@ import { TipoHabitacionService } from '../../api/tipo.habitacion.service';
   standalone: true,
   templateUrl: './consultar-disponibilidad-habitaciones.component.html',
   styleUrl: './consultar-disponibilidad-habitaciones.component.scss',
-  imports: [ FooterComponent,CommonModule,FormsModule]
+  imports: [FooterComponent, CommonModule, FormsModule]
 })
 export class ConsultarDisponibilidadHabitacionesComponent implements OnInit {
   token: string | null;//token de session
-  checkOutDesactivado : boolean;
-  checkInDesactivado : boolean;
-  tipo_habitacionDesactivado:boolean;
+  checkOutDesactivado: boolean;
+  checkInDesactivado: boolean;
+  tipo_habitacionDesactivado: boolean;
   formData: any = [];//acceso a los input del formulario
   mensajeError = '';
   mostrarError = false;
   mostrarConfirmacion = false;
   mensajeConfirmacion = '';
-  tiposDeHabitacion : TipoHabitacion[]=[];//todos los tipos de habitaciones disponibles
-  constructor( private router: Router, private service: TipoHabitacionService) {
+  tiposDeHabitacion: TipoHabitacion[] = [];//todos los tipos de habitaciones disponibles
+  habitacionesDisponibles: Habitacion[] = [];
+
+  constructor(private router: Router, private service: TipoHabitacionService) {
 
     //para resguardar ruta
     if (typeof localStorage !== 'undefined') {
@@ -37,12 +47,12 @@ export class ConsultarDisponibilidadHabitacionesComponent implements OnInit {
     }
     this.checkInDesactivado = false;
     this.checkOutDesactivado = true;
-    this.tipo_habitacionDesactivado=true;
-    
+    this.tipo_habitacionDesactivado = true;
+
   }
 
-  ngOnInit(): void{
-    
+  ngOnInit(): void {
+
     //verificar autenticacion
     if (this.token == null) {
       this.router.navigate(['/login']);
@@ -52,54 +62,54 @@ export class ConsultarDisponibilidadHabitacionesComponent implements OnInit {
       this.tiposDeHabitacion = data;
     });
   }
-  
+
   ////////////////////////////////////validaciones
-  mostrarErrorCheckIn=false;
+  mostrarErrorCheckIn = false;
   validarCheckIn() {
     this.mostrarErrorCheckIn = !this.formData.checkIn; // Si this.formData.checkIn está vacío, mostrarErrorCheckIn será true
     return;
   }
 
-  mostrarErrorCheckOut=false;
+  mostrarErrorCheckOut = false;
   validarCheckOut() {
     this.mostrarErrorCheckOut = !this.formData.checkOut; // Si this.formData.checkOut está vacío, mostrarErrorCheckOut será true
     return;
   }
-  mostrarErrorTipoHabitacion=false;
+  mostrarErrorTipoHabitacion = false;
   validarTipoHabitacion() {
     this.mostrarErrorTipoHabitacion = !this.formData.tipo_habitacion; // Si this.formData.tipo_habitacion está vacío, mostrarErrorTipoHabitacion será true
     return;
   }
-  validarRequerido(){
+  validarRequerido() {
     this.validarCheckIn();
     this.validarCheckOut();
     this.validarTipoHabitacion();
   }
 
   //validar que la reserva sea del día actual o posterior, que no sea un día pasado
-  validarFechaEntrada(event: Event){
+  validarFechaEntrada(event: Event) {
     //const fechaActualCR = moment().tz('America/Costa_Rica').format('YYYY-MM-DD');
     const fechaActual = new Date().toISOString().split('T')[0];
     const fechaLlegada = new Date(this.formData.checkIn); // Obtener la fecha de llegada a las 00:00:00
-   
+
     // Validar que la fecha de llegada sea posterior o igual al día actual
     if (fechaLlegada.toISOString().slice(0, 10) === fechaActual) {//si es el día de hoy
       this.mostrarConfirmacion = true;
-      this.mensajeConfirmacion='Ell check in es a partir de las 3 pm.';
-      this.mostrarError=false;
-      this.checkOutDesactivado=false;
-      this.checkInDesactivado=true;
+      this.mensajeConfirmacion = 'El check in es a partir de las 3 pm.';
+      this.mostrarError = false;
+      this.checkOutDesactivado = false;
+      this.checkInDesactivado = true;
     } else if (fechaLlegada.toISOString().slice(0, 10) < fechaActual) {//si elije un día pasado
-      this.mostrarError=true;
+      this.mostrarError = true;
       this.mostrarConfirmacion = false;
-      this.mensajeError = 'La fecha de llegada debe ser el día de '+fechaActual+' o posterior';
-      
-    }else{//si elije un día futuro
-      this.mostrarError=false;
+      this.mensajeError = 'La fecha de llegada debe ser el día de ' + fechaActual + ' o posterior';
+
+    } else {//si elije un día futuro
+      this.mostrarError = false;
       this.mostrarConfirmacion = true;
-      this.mensajeConfirmacion='El check in para el día '+fechaLlegada.toISOString().slice(0, 10)+' es a partir de las 3 pm.';
-      this.checkOutDesactivado=false;
-      this.checkInDesactivado=true;
+      this.mensajeConfirmacion = 'El check in para el día ' + fechaLlegada.toISOString().slice(0, 10) + ' es a partir de las 3 pm.';
+      this.checkOutDesactivado = false;
+      this.checkInDesactivado = true;
     }
     this.validarCheckIn();
   }
@@ -108,28 +118,63 @@ export class ConsultarDisponibilidadHabitacionesComponent implements OnInit {
   mostrarError_ = false;
   mostrarConfirmacion_ = false;
   mensajeConfirmacion_ = '';
-//validar fecha de salida
-validarFechaDeSalida(event: Event){
-  const fechaLlegada = new Date(this.formData.checkIn); // Obtener la fecha de llegada a las 00:00:00
-  const fechaSalida = new Date(this.formData.checkOut); // Obtener la fecha de salida a las 00:00:00
+  //validar fecha de salida
+  validarFechaDeSalida(event: Event) {
+    const fechaLlegada = new Date(this.formData.checkIn); // Obtener la fecha de llegada a las 00:00:00
+    const fechaSalida = new Date(this.formData.checkOut); // Obtener la fecha de salida a las 00:00:00
 
-  // Validar que la fecha de salida sea posterior a la fecha de llegada
-  if(!this.formData.checkIn){
-    this.mensajeError_='Primero elije la fecha de entrada';
-    this.mostrarError_=true;
-    this.mostrarConfirmacion_=false;
-  }else if (fechaSalida <= fechaLlegada) {
-    this.mensajeError_='La fecha de salida debe ser posterior a la fecha de llegada.';
-    this.mostrarError_=true;
-    this.mostrarConfirmacion_=false;
-  }else{
-    this.mensajeConfirmacion_='El día ' + fechaSalida.toISOString().slice(0, 10) + ' a las 11 am se hace check out';
-    this.mostrarError_=false;//no hay error
-    this.mostrarConfirmacion_=true;//se confirma la hora recivido
-    this.checkOutDesactivado = true;//se desactiva el campo check out
-    this.tipo_habitacionDesactivado=false;//se activa el tipo de habtacion
+    // Validar que la fecha de salida sea posterior a la fecha de llegada
+    if (!this.formData.checkIn) {
+      this.mensajeError_ = 'Primero elije la fecha de entrada';
+      this.mostrarError_ = true;
+      this.mostrarConfirmacion_ = false;
+    } else if (fechaSalida <= fechaLlegada) {
+      this.mensajeError_ = 'La fecha de salida debe ser posterior a la fecha de llegada.';
+      this.mostrarError_ = true;
+      this.mostrarConfirmacion_ = false;
+    } else {
+      this.mensajeConfirmacion_ = 'El día ' + fechaSalida.toISOString().slice(0, 10) + ' a las 11 am se hace check out';
+      this.mostrarError_ = false;//no hay error
+      this.mostrarConfirmacion_ = true;//se confirma la hora recivido
+      this.checkOutDesactivado = true;//se desactiva el campo check out
+      this.tipo_habitacionDesactivado = false;//se activa el tipo de habtacion
+    }
+    this.validarCheckOut();
   }
-  this.validarCheckOut();
-}
 
+  cantidadNoches = 0;
+  contarNoches() {
+    if (this.formData.checkIn && this.formData.checkOut) {
+      const fechaInicioParsed = parseISO(this.formData.checkIn); // Parsea la fecha de inicio
+      const fechaFinParsed = parseISO(this.formData.checkOut); // Parsea la fecha de fin
+      this.cantidadNoches = differenceInDays(fechaFinParsed, fechaInicioParsed); // Calcula la diferencia en días
+    } else {
+      this.cantidadNoches = 0; // Reinicia la cantidad de noches si las fechas no están completas
+    }
+  }
+
+  consultar() {
+    this.validarRequerido();
+    if (
+      this.mostrarErrorTipoHabitacion
+      || this.mostrarErrorCheckIn
+      || this.mostrarErrorCheckOut
+      || this.mostrarError
+      || this.mostrarError_
+    ) {
+      return;
+    }
+
+
+    this.service.obtenerHabitacionesDisponibles(this.formData.checkIn, this.formData.checkOut, this.formData.tipo_habitacion)
+      .subscribe((data) => {
+        this.habitacionesDisponibles = data;
+        this.contarNoches();
+        alert('Cantidad de días: ' + this.cantidadNoches);
+        this.habitacionesDisponibles.forEach(habitacion => {
+          alert(habitacion.numero);
+        });
+
+      });
+  }
 }
