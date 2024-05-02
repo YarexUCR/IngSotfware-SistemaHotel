1ee7567068944c51ff37b0811b18a35de1d4791e
kjgCH1 <cheskyto22@gmail.com>
2024-05-02 00:30:32 -0600

    consumir el servicio nuevo desde angular
    tipoHabitacion

--- frontend-Angular17/app/src/app/api/tipo.habitacion.service.ts
+++ frontend-Angular17/app/src/app/api/tipo.habitacion.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient,HttpHeaders } from "@angular/common/http";
+import { HttpClient,HttpParams } from "@angular/common/http";
 import { Injectable, inject } from "@angular/core";
 import { Observable } from "rxjs";
 import { TipoHabitacion } from "../dominio/TipoHabitacion";
@@ -9,4 +9,13 @@ export class HotelService{
     getTiposHabitaciones():Observable<TipoHabitacion[]>{
          return this._http.get<TipoHabitacion[]>('https://localhost:7200/TipoHabitacion');
     }
+
+    obtenerHabitacionesDisponibles(checkIn: string, checkOut: string, tipoHabitacion: number): Observable<any> {
+        const params = new HttpParams()
+          .set('checkIn', checkIn)
+          .set('checkOut', checkOut)
+          .set('tipoHabitacion', tipoHabitacion.toString());
+    
+        return this._http.get('https://localhost:7200/TipoHabitacion/ObtenerHabitacionesDisponiblesParaReserva', { params });
+      }
 }
