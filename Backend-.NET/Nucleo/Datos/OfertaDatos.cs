9c48483e7cbd60af12d0ebae9e9a35b487551a59
Yarex17 <159191461+YarexUCR@users.noreply.github.com>
2024-05-03 11:29:44 -0600

    dddd

---
+++ Backend-.NET/Nucleo/Datos/OfertaDatos.cs
@@ -0,0 +1,32 @@
+﻿using Dominio;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos
+{
+    public class OfertaDatos
+    {
+        private readonly string _connectionString;
+        public OfertaDatos(IConfiguration configuration)
+        {
+
+            _connectionString = configuration.GetConnectionString("HotelPalmConexion");
+
+        }
+
+
+        public async Task<List<Oferta>> getAllOfertas()
+        {
+
+
+            return new List<Oferta>();
+        }
+
+
+
+    }
+}
