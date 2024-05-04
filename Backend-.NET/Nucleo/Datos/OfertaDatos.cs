e9659c669e20a0346b05ccb24270928fcf88cede
Yarex17 <159191461+YarexUCR@users.noreply.github.com>
2024-05-03 22:10:16 -0600

    ddddddd

--- Backend-.NET/Nucleo/Datos/OfertaDatos.cs
+++ Backend-.NET/Nucleo/Datos/OfertaDatos.cs
@@ -43,9 +43,9 @@ namespace Datos
                             Id = Convert.ToInt32(reader["id"]),
                             Nombre = Convert.ToString(reader["nombre"]),
                             Descuento = Convert.ToInt32(reader["descuento"]),
-                            FechaInicio = Convert.ToDateTime(reader["fechaInicio"]),
-                            FechaFin = Convert.ToDateTime(reader["fechaFin"]),
-                            Activo = Convert.ToBoolean(reader["activo"])
+                            //FechaInicio = Convert.ToDateTime(reader["fechaInicio"]),
+                           // FechaFin = Convert.ToDateTime(reader["fechaFin"]),
+//Activo = Convert.ToBoolean(reader["activo"])
                             // Mapea otros campos según tu estructura de datos
                         };
 
@@ -57,10 +57,42 @@ namespace Datos
                 }
             }
 
-            return habitacionesDisponibles;
+            return ofertas;
         }
 
+        public async Task<List<TipoHabitacion>> getTipoDeHabitcion(int id) {
 
+            List<TipoHabitacion> TipoHabitacion = new List<TipoHabitacion>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand command = new SqlCommand("BuscarTipoHabitacionPorOferta", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@checkIn", id);
+                   
+
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        TipoHabitacion tipoHabitacion = new TipoHabitacion
+                        {
+                            Id = Convert.ToInt32(reader["id"]),
+                            Nombre = Convert.ToString(reader["descripcion"]),
+                        
+                        };
+
+                        TipoHabitacion.Add(tipoHabitacion);
+                    }
+
+                    reader.Close();
+                }
+            }
+
+            return TipoHabitacion;
+        }
       
 
 
--- Backend-.NET/Nucleo/Dominio/Oferta.cs
+++ Backend-.NET/Nucleo/Dominio/Oferta.cs
@@ -1,4 +1,4 @@
-﻿viusing System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
