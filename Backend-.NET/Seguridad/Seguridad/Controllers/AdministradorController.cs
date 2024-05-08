279c011a2783d9125b0ee07e1796d2a6ab47b240
Yarex17 <159191461+YarexUCR@users.noreply.github.com>
2024-05-07 18:35:50 -0600

    tttt

--- Backend-.NET/Seguridad/Seguridad/Controllers/AdministradorController.cs
+++ Backend-.NET/Seguridad/Seguridad/Controllers/AdministradorController.cs
@@ -16,7 +16,7 @@ namespace Seguridad.Controllers
         [HttpGet]
         public IActionResult ObtenerNombreAdministrador(string nombreUsuario, string contrasena) {
            
-            return Ok(JsonConvert.SerializeObject(_admnistradorDatos.ObtenerNombreAdministrador(nombreUsuario, contrasena)))a;
+            return Ok(JsonConvert.SerializeObject(_admnistradorDatos.ObtenerNombreAdministrador(nombreUsuario, contrasena)));
         }
     }
 }
